1b3a902ce56c4144a7d8433ca2ae2b4986b601b4
bkohler616 <bkohler616@hotmail.com>
2016-04-11 10:25:14 -0400

    Setup Items.

---
+++ Index.php
@@ -0,0 +1,14 @@
+<HTML>
+<head>
+    <?
+    require_once("cssItems.php");
+    ?>
+</head>
+<body>
+
+</body>
+
+<?php
+require_once("jsItems.php");
+?>
+</HTML>
---
+++ cssItems.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap-theme.min.css">
