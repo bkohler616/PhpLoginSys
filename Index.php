97c9466bf529bdcb6fc99105b26bf770f544557e
bkohler616 <bkohler616@hotmail.com>
2016-04-18 10:13:49 -0400

    Revert "Feature nav bar"

--- Index.php
+++ Index.php
@@ -1,14 +1,11 @@
-<?php $activePage = "Index" ?>
-<!DOCTYPE HTML>
 <HTML>
 <head>
-    <?php
+    <?
     require_once("cssItems.php");
     ?>
 </head>
 <body>
-<?php require_once("NavBar.php"); ?>
-Hello World!
+
 </body>
 
 <?php
